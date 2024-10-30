ee500d1a730f19bf26860f540fd3790cff9cda2a
Jonathan Ixcayau <jonynija@gmail.com>
2024-10-30 12:18:40 -0500

    Add heart text

--- lib/main.dart
+++ lib/main.dart
@@ -109,7 +109,7 @@ class _MyHomePageState extends State<MyHomePage> {
               'You have pushed the button this many times:',
             ),
             Text(
-              '$_counter',
+              '<3 $_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
